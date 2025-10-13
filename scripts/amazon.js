36d931b94a5873ef7473786542c62c19ae1b8c0e
Rahul-krishh <rahulmykris@gmail.com>
2025-10-13 21:46:28 +0530

    feat : made product add btn interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,3 @@
-const products = [ {
-  image: 'images/main/product-image/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  },
-  {
-  image: 'images/main/product-image/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095,
-  },
-  {
-  image: 'images/main/product-image/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 790,
-  },
-]
-
 let productsHTML = '';
 
 products.forEach( (product) => {
@@ -34,7 +5,9 @@ products.forEach( (product) => {
   productsHTML += `
     <article class="hero-section__product-card">
         
-        <img class="hero-section__product-card-image" src="${product.image}" alt="Product-Image">
+        <div class="hero-section__product-card-image-cont">
+          <img class="hero-section__product-card-image" src="${product.image}" alt="Product-Image">
+        </div>
         <h2 class="hero-section__product-card-text">${product.name}</h2>
         
         <div class="hero-section__product-card-rating-box">
@@ -56,10 +29,39 @@ products.forEach( (product) => {
               <option value="9">9</option>
               <option value="10">10</option>
         </select>
-        <button class="hero-section__product-card-add-btn">Add to Cart</button>
+        <button class="hero-section__product-card-add-btn js-product-card-add-btn" data-product-id = "${product.id}">Add to Cart</button>
       
       </article>
   `
 })
 
 document.querySelector('.js-hero-section__wrapper').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-product-card-add-btn').forEach( (button) => {
+  button.addEventListener( 'click' , () => {
+    const productId = button.dataset.productId;
+
+    //----product added already ?-----
+
+    let matchedItem;
+
+            // check cart, if doesnt have assign the variable matched 
+    cart.forEach( (item) => {
+      if(productId === item.productId) {
+        matchedItem = item;
+      }
+    });
+
+           // if matchede as valye in if(matchedItem) becomes truthy so quantity increases by 1
+    if(matchedItem) {
+      matchedItem.quantity++;
+    } else {
+      cart.push({
+        productId : productId,
+        quantity : 1
+      });
+    }
+
+    console.log(cart);
+  })
+});
